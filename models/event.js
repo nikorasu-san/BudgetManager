ab172acc7dacce25de81bcf7fde4e607c8ce8d2e
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-04-11 08:16:46 -0500

    event model complete, testing to begin shortly

--- models/event.js
+++ models/event.js
@@ -1,75 +1,40 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
+  var Event = sequelize.define("User", {
     // how app will address the user
-    preferedName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    monthlyIncome: {
-      type: DataTypes.DECIMAL(12, 2),
-      defaultValue: 1000
-    },
-    // email is required as it is used for login
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: { len: [1] }
-    },
-    // if user would like email updates
-    emailFlag: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    phone: {
+    description: {
       type: DataTypes.STRING,
       //   allowNull: false,
-      validate: { len: [7] }
+      validate: {
+        len: [1]
+      }
     },
-    // if user would like text updates
-    phoneFlag: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
+    category: {
+      type: DataTypes.INTEGER(2),
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     },
-    // the user is invited to name their own budget categories
-    cat0name: {
-      type: DataTypes.STRING,
-      defaultValue: "Spending",
-      validate: { len: [1] }
-    },
-    cat0cap: {
+    amount: {
       type: DataTypes.DECIMAL(10, 2),
       defaultValue: 1000
     },
-    // The other categories are optional, for slicing the pie (chart)
-    cat1name: DataTypes.STRING,
-    cat1cap: DataTypes.DECIMAL(10, 2),
-    cat2name: DataTypes.STRING,
-    cat2cap: DataTypes.DECIMAL(10, 2),
-    cat3name: DataTypes.STRING,
-    cat3cap: DataTypes.DECIMAL(10, 2),
-    cat4name: DataTypes.STRING,
-    cat4cap: DataTypes.DECIMAL(10, 2),
-    cat5name: DataTypes.STRING,
-    cat5cap: DataTypes.DECIMAL(10, 2),
-    cat6name: DataTypes.STRING,
-    cat6cap: DataTypes.DECIMAL(10, 2),
-    cat7name: DataTypes.STRING,
-    cat7cap: DataTypes.DECIMAL(10, 2),
-    cat8name: DataTypes.STRING,
-    cat8cap: DataTypes.DECIMAL(10, 2),
-    cat9name: DataTypes.STRING,
-    cat9cap: DataTypes.DECIMAL(10, 2),
-    // should the user wish to 'delete' their account, it will be deactivated
+    // optional entry field for future or past events, otherwise we will take CREATED AT
+    date: DataTypes.DATEONLY,
+    // is bill(true means event has not yet occured; funds not yet transferred)
+    billFlag: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    // if the Bill is recurring
+    isRecurring: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    // recurrence periodicity as string to parse
+    periodicity: DataTypes.STRING,
+
+    // should the user wish to cancel the event, it will be deactivated
     activeFlag: {
       type: DataTypes.BOOLEAN,
       defaultValue: true
