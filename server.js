f41cff12174bb949992b7df49e47d68dfb434ee1
BlackMG27 <oike279@gmail.com>
2019-04-10 23:08:12 -0500

    added entries, profile, signup handlebars files to the project

---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
