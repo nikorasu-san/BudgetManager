623e12306f61319f99ae868f32615fa56b43118d
neagtivefriction <chris.chrispete@gmail.com>
2019-04-12 14:31:26 -0500

    Added in the basic cookie function. Need to do much more testing, so it isn't called by anything at this time.

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,64 +1,65 @@
-$(document).ready(function () {
-    $(document).on("click", "#loginbtn", function () {
-        let email = $("#email").val();
-        let password = $("#password").val();
+$(document).ready(function() {
+  // var uid = checkCookie();
+  // // Check to see if the user has a login id.
 
-        // validate if email
-        if (email.trim() === "") {
-            alert("Please add in an email")
+  // if (uid){
+  //     // Code to redirect them to the dashboard.
+  // }
+  $(document).on("click", "#loginbtn", function() {
+    let email = $("#email").val();
+    let password = $("#password").val();
+
+    // validate if email
+    if (email.trim() === "") {
+      alert("Please add in an email");
+    } else if (password.length < 6) {
+      alert("Please enter a password of at least 6 characters");
+    } else {
+      let userDetails = {
+        email: email.trim(),
+        password: password.trim()
+      };
+      console.log(userDetails);
+      $.post("/login", userDetails).then(function(data) {
+        if (data) {
+          window.location.href = "/dashboard";
         }
-        else if (password.length < 6) {
-            alert("Please enter a password of at least 6 characters")
-        } else {
+      });
+      // test if we can redirect on front end
+      // document.location.href = "/";
+    }
+  });
 
-            let userDetails = {
-                email: email.trim(),
-                password: password.trim()
-            }
-            console.log(userDetails);
-            $.post("/login", userDetails).then(function (data) {
-                if (data) {
-                    window.location.href = "/dashboard";
-                }
-            })
-            // test if we can redirect on front end
-            // document.location.href = "/";
+  $(document).on("click", "#newUser", function() {
+    let preferredName = $("#preferredName").val();
+    let email = $("#email").val();
+    let phoneNumber = $("#phoneNumber").val();
+    let password = $("#password").val();
+    console.log("name:", preferredName);
+
+    // validate if email is not blank
+    if (email.trim() === "") {
+      alert("Please add in an email");
+    } else if (preferredName.trim() === "") {
+      alert("Please add in a name");
+    } else if (password.length < 6) {
+      alert("Please enter a password of at least 6 characters");
+    } else {
+      let newUser = {
+        preferredName: preferredName.trim(),
+        email: email.trim(),
+        phoneNumber: phoneNumber,
+        password: password
+      };
+      console.log(newUser);
+      $.post("/signup", userDetails).then(function(data) {
+        if (data.id) {
+          window.location.href = "/dashboard";
+        } else if (data.error) {
+          let message = data.error;
+          alert(message);
         }
-    })
-
-    $(document).on("click", "#newUser", function () {
-        let preferredName = $("#preferredName").val();
-        let email = $("#email").val();
-        let phoneNumber = $("#phoneNumber").val();
-        let password = $("#password").val();
-        console.log("name:", preferredName)
-
-        // validate if email is not blank
-        if (email.trim() === "") {
-            alert("Please add in an email")
-        } else if (preferredName.trim() === "") {
-            alert("Please add in a name")
-        } else if (password.length < 6) {
-            alert("Please enter a password of at least 6 characters")
-        } else {
-            let newUser = {
-                preferredName: preferredName.trim(),
-                email: email.trim(),
-                phoneNumber: phoneNumber,
-                password: password
-            }
-            console.log(newUser);
-            $.post("/signup", userDetails).then(function (data) {
-                if (data.id) {
-                    window.location.href = "/dashboard";
-                } else if (data.error) {
-                    let message = data.error;
-                    alert(message);
-                }
-            })
-
-        }
-    })
-
-
+      });
+    }
+  });
 });
