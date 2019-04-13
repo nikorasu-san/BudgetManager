3d501aa3b004a780b3a940f660f475246b626db0
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-04-13 11:15:30 -0500

    commenting out utils for clarity

--- utils/get13.js
+++ utils/get13.js
@@ -4,12 +4,13 @@ let catNames;
 let catCaps;
 let catWarns;
 var get13 = function(userid, callback) {
-  console.log(userid);
-
+  // console.log(userid);
+  // // get the data from user table
   db.User.findOne({
     where: { id: userid.uid }
   }).then(x => {
-    // console.log("returned user object", x.dataValues);
+    //// we could change these initializations to loops based on number of categories if they were dynamic
+    //// but since I had typed it out once it seemed faster to copy an paste
     catNames = [
       x.dataValues.cat0name,
       x.dataValues.cat1name,
@@ -49,25 +50,35 @@ var get13 = function(userid, callback) {
     // console.log("arr of names", catNames);
     // console.log("arr of caps", catCaps);
     // console.log("arr of warns", catWarns);
+    // // get all active spending events of user
     db.Event.findAll({
-      where: { userId: userid.uid, activeFlag: true }
+      where: {
+        userId: userid.uid,
+        activeFlag: true
+      }
     }).then(y => {
+      // // initialize some arrays
       let catTotalFloats = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
       let catCapFloats = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
       let catWarnFloats = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      // // loop through all events, and add each event amount to the appropriate total array
+      // // position using category id as index
       y.forEach(v => {
         let w = v.dataValues;
         catTotalFloats[w.category] += parseFloat(w.amount);
       });
+      // // now that all events or sorted, do the math on the categories
       catTotalFloats.forEach((v, i) => {
         // console.log("i", i, catCaps[i]);
         // console.log("float", i, parseFloat(catCaps[i]));
         // console.log(v);
 
-        catCapFloats[i] = (v / parseFloat(catCaps[i])).toFixed(3);
+        catCapFloats[i] = (v / parseFloat(catCaps[i])).toFixed(2);
 
         catWarnFloats[i] = (v / parseFloat(catWarns[i])).toFixed(3);
       });
+      // // returning identically indexed arrays of category names, budget caps, warning levels,
+      // // totals, and totals' percentage of budget caps and warning levels
       let returnOb = {
         catNames,
         catCaps,
