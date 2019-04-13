e66ee6671f7269cc130f58719262f015113d8a00
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-04-13 11:51:25 -0500

    utils commented out, next step will be date support

--- utils/put12.js
+++ utils/put12.js
@@ -3,11 +3,12 @@ var db = require("../models");
 
 var put12 = function(entryObject, callback) {
   let eid = entryObject.eid;
+  // // rather than delete a bill, we deactivate it
   let insertObject = {
     activeFlag: false
   };
   db.Event.update(insertObject, { where: { id: eid } }).then(response => {
-    console.log("put12", response);
+    // console.log("put12", response);
     callback(response);
   });
 };
--- utils/put14.js
+++ utils/put14.js
@@ -3,11 +3,12 @@ var db = require("../models");
 
 var put14 = function(entryObject, callback) {
   let uid = entryObject.uid;
+  // // here the wonderous sorcery of computing properties makes assigning budget caps and warning levels not a total nightmare :)
   let insertOb = {
     [`cat${entryObject.category}cap`]: entryObject.capAmount,
     [`cat${entryObject.category}warn`]: entryObject.warnAmount
   };
-  console.log(insertOb);
+  // console.log("put14 inOb", insertOb);
 
   db.User.update(insertOb, {
     where: {
@@ -18,5 +19,6 @@ var put14 = function(entryObject, callback) {
   });
 };
 module.exports = put14;
-
-// Either var obj = {[`${dyanmicKey}`]: val} or just var obj = {[dyanmicKey]: val}
+// // N.B.: dynamic property computation examples from  stack overflow comment by – Bergi May 31 '16 at 14:14
+// // (https://stackoverflow.com/questions/33194138/template-string-as-object-property-name)
+// // "Either var obj = {[`${dyanmicKey}`]: val} or just var obj = {[dyanmicKey]: val}"
