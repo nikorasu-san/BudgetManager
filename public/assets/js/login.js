680372fdd4d250be02dc25aa3b7822961cd32d31
BlackMG27 <oike279@gmail.com>
2019-04-15 13:23:13 -0500

    resolved conflicts

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -56,65 +56,62 @@ $(document).ready(function () {
 });
 
 $(document).on("click", "#newUser", function (event) {
-event.preventDefault()
-let preferredName = $("#preferredName").val();
-let email = $("#email").val();
-let phoneNumber = $("#phoneNumber").val();
-// if validation on backend is still on to require length 10 -- set a default value
-// if (phoneNumber.length < 10) {
-//     phoneNumber = 5555555555;
-// }
-let password = $("#password").val();
+  event.preventDefault()
+  let preferredName = $("#preferredName").val();
+  let email = $("#email").val();
+  let phoneNumber = $("#phoneNumber").val();
+  // if validation on backend is still on to require length 10 -- set a default value
+  // if (phoneNumber.length < 10) {
+  //     phoneNumber = 5555555555;
+  // }
+  let password = $("#password").val();
 
-// validate if email is not blank
-if (email.trim() === "") {
-// open error modal
-$('.modal').modal();
-$('#error-body').empty()
-$('#error-body').append("<p>Please add in an email</p>");
-//alert("Please add in an email");
-} else if (preferredName.trim() === "") {
-// open error modal
-$('.modal').modal();
-$('#error-body').empty()
-$('#error-body').append("<p>Please add in a name</p>");
-//alert("Please add in a name");
-} else if (password.length < 6) {
-// open error modal
-$('.modal').modal();
-$('#error-body').empty()
-$('#error-body').append("<p>Please enter a password of at least 6 characters</p>");
-//alert("Please enter a password of at least 6 characters");
-} else {
-let newUser = {
-  preferredName: preferredName.trim(),
-  email: email.trim(),
-  phoneNumber: phoneNumber,
-  password: password
-};
-console.log(newUser);
-$.post("/signup", newUser).then(function (data) {
-  console.log("data: ", data)
-  if (data.id) {
-    // set local storage as backup
-    localStorage.setItem("budget_user_id", data.id);
-    location.replace("/" + data.id)
-  } else if (data.error) {
-    let message = data.error;
+  // validate if email is not blank
+  if (email.trim() === "") {
     // open error modal
     $('.modal').modal();
     $('#error-body').empty()
-    $('#error-body').append(`<p>${message}</p>`);
-    //alert(message);
+    $('#error-body').append("<p>Please add in an email</p>");
+    //alert("Please add in an email");
+  } else if (preferredName.trim() === "") {
+    // open error modal
+    $('.modal').modal();
+    $('#error-body').empty()
+    $('#error-body').append("<p>Please add in a name</p>");
+    //alert("Please add in a name");
+  } else if (password.length < 6) {
+    // open error modal
+    $('.modal').modal();
+    $('#error-body').empty()
+    $('#error-body').append("<p>Please enter a password of at least 6 characters</p>");
+    //alert("Please enter a password of at least 6 characters");
   } else {
-    // open error modal
-    $('.modal').modal();
-    $('#error-body').empty()
-    $('#error-body').append(`<p>There was an error with this request. Please make sure you have filled out the form correctly.</p>`);
+    let newUser = {
+      preferredName: preferredName.trim(),
+      email: email.trim(),
+      phoneNumber: phoneNumber,
+      password: password
+    };
+    console.log(newUser);
+    $.post("/signup", newUser).then(function (data) {
+      console.log("data: ", data)
+      if (data.id) {
+        // set local storage as backup
+        localStorage.setItem("budget_user_id", data.id);
+        location.replace("/" + data.id)
+      } else if (data.error) {
+        let message = data.error;
+        // open error modal
+        $('.modal').modal();
+        $('#error-body').empty()
+        $('#error-body').append(`<p>${message}</p>`);
+        //alert(message);
+      } else {
+        // open error modal
+        $('.modal').modal();
+        $('#error-body').empty()
+        $('#error-body').append(`<p>There was an error with this request. Please make sure you have filled out the form correctly.</p>`);
+      }
+    });
   }
-});
-}
-});
-}
-});
 });
